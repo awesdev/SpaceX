fa6350a5f77b8aa817207158e593a9da8052ce1e
Kirill <kirillkolesnichenko21@gmail.com>
2021-11-21 21:31:50 +0300

    added burger-menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     new WOW().init();
+
+    $(".burger-menu").on("click", function () {
+        $(this).toggleClass("burger-menu--active");
+        $(".header__menu").toggleClass("header__menu--active");
+        $(".header__menu").toggleClass("wow fadeInRight");
+    })
 })
